d903cf186c24dcedc51eea40f3473114b6fa6918
Haziq Bangash <72706449+haziq-bangash@users.noreply.github.com>
2022-11-11 19:40:15 +0500

    Frontend project 2 (#3)
    * frontend-project-2
    
    * Update README.md

---
+++ frontend-project-2/src/components/modules/components/Intro_Video.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Intro_Video = () => {
+  return (
+    <div>Intro_Video</div>
+  )
+}
+
+export default Intro_Video
---
+++ frontend-project-2/src/components/modules/components/Recommended_Roles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recommended_Roles = () => {
+  return (
+    <div>Recommended_Roles</div>
+  )
+}
+
+export default Recommended_Roles
